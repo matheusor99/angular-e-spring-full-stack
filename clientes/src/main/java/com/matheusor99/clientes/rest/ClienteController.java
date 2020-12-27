d0a924e5cd9c9ebc2c6a6c65cab137afee7800d9
matheusor99 <matheusor99@gmail.com>
2020-12-27 11:54:24 -0300

    buscando cliente pelo id

--- clientes/src/main/java/com/matheusor99/clientes/rest/ClienteController.java
+++ clientes/src/main/java/com/matheusor99/clientes/rest/ClienteController.java
@@ -2,11 +2,14 @@ package com.matheusor99.clientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.matheusor99.clientes.model.entitys.Cliente;
 import com.matheusor99.clientes.model.repository.ClienteRepository;
@@ -27,4 +30,21 @@ public class ClienteController {
 	public Cliente salvar( @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
+	
+	@GetMapping("/{id}")
+	public Cliente acharPorId(@PathVariable Integer id) {
+		return clienteRepository
+				.findById(id)
+				.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
 }
+
+
+
+
+
+
+
+
+
+
