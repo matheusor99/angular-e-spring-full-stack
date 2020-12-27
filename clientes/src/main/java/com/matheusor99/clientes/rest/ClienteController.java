1703424d91e790cd5b0f6191c0576d55c46e803a
matheusor99 <matheusor99@gmail.com>
2020-12-27 12:01:48 -0300

    deletando cliente

--- clientes/src/main/java/com/matheusor99/clientes/rest/ClienteController.java
+++ clientes/src/main/java/com/matheusor99/clientes/rest/ClienteController.java
@@ -2,6 +2,7 @@ package com.matheusor99.clientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,18 @@ public class ClienteController {
 		return clienteRepository
 				.findById(id)
 				.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deletar(@PathVariable Integer id) {
+		clienteRepository
+			.findById(id)
+			.map(cliente -> {
+				clienteRepository.delete(cliente);
+				return Void.TYPE;
+			})
+			.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
 }
 
