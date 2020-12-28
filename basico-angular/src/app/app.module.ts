fe93402ae05c3b1e6340f2f20038dd2f6ba77b4b
matheusor99 <matheusor99@gmail.com>
2020-12-28 13:27:28 -0300

    basico de angular

--- basico-angular/src/app/app.module.ts
+++ basico-angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { CursoAngularComponent } from './curso-angular/curso-angular.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    CursoAngularComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule
