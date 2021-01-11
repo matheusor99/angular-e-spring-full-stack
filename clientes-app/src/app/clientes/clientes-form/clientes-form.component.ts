5fb750b2d82fa95b68b7a118a053149f41e96d36
matheusor99 <matheusor99@gmail.com>
2021-01-11 13:08:10 -0300

    corrigindo erro de parametros

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { ClientesService } from 'src/app/clientes.service';
 import { Cliente } from '../cliente';
 
@@ -24,16 +25,19 @@ export class ClientesFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let params = this.activatedRoute.params;
-    if(params && params.value && params.value.id) {
-      this.id = params.value.id;
-      this.service.getClienteById(this.id)
-        .subscribe( response => {
-          this.cliente = response;
-        }, error => {
-          this.cliente = new Cliente();
-        })
-    }
+    let params : Observable<Params> = this.activatedRoute.params;
+    params.subscribe( urlParams => {
+      this.id = urlParams['id'];
+      if(this.id) {
+        this.service
+          .getClienteById(this.id)
+          .subscribe( response => {
+            this.cliente = response;
+          }, error => {
+            this.cliente = new Cliente();
+          })
+      }
+    })
   }
 
   onSubmit() {
