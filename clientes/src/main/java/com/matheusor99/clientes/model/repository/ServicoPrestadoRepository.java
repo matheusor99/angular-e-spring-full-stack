f5ca7d8f179d7a214d3c94e53a7b8aac5a54f19f
matheusor99 <matheusor99@gmail.com>
2021-01-11 14:04:49 -0300

    atualizar nome da classe

---
+++ clientes/src/main/java/com/matheusor99/clientes/model/repository/ServicoPrestadoRepository.java
@@ -0,0 +1,8 @@
+package com.matheusor99.clientes.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheusor99.clientes.model.entitys.ServicoPrestado;
+
+public interface ServicoPrestadoRepository extends JpaRepository<ServicoPrestado, Integer>{
+}
--- clientes/src/main/java/com/matheusor99/clientes/model/repository/ServicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.matheusor99.clientes.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.matheusor99.clientes.model.entitys.Servico;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer>{
-	
-}
