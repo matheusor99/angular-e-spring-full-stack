6c6f19b38991ea454b68dc405fb1da5d289912d8
matheusor99 <matheusor99@gmail.com>
2021-02-24 00:29:57 -0300

    serviços prestados pesquisar

--- clientes/src/main/java/com/matheusor99/clientes/rest/ServicoPrestadoController.java
+++ clientes/src/main/java/com/matheusor99/clientes/rest/ServicoPrestadoController.java
@@ -2,12 +2,15 @@ package com.matheusor99.clientes.rest;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -47,5 +50,13 @@ public class ServicoPrestadoController {
 		servicoPrestado.setValor( bigDecimalConverter.converter(dto.getValor()) );
 		
 		return servicoPrestadoRepository.save(servicoPrestado);
+	}
+	
+	@GetMapping
+	public List<ServicoPrestado> pesquisar(
+			@RequestParam(value="nome", required = false, defaultValue = "") String nome,
+			@RequestParam(value="mes", required = false) Integer mes
+	) {
+		return servicoPrestadoRepository.findByNomeClienteAndMes("%" + nome + "%", mes);
 	}
 }
